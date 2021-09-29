6cbb9c1e244814b4b8eb4a0ea2f1aac0c63a0f64
trje9656 mac <trje9656@colorado.edu>
2021-09-29 16:53:00 -0600

    Added all relevant buttons, steppers, and labels for main app design Added keyboard dismissal functionality

--- Lab4_RPGCharacterMaker/ViewController.swift
+++ Lab4_RPGCharacterMaker/ViewController.swift
@@ -7,9 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var strengthStepper: UIStepper!
+    @IBOutlet weak var intelStepper: UIStepper!
+    @IBOutlet weak var dexStepper: UIStepper!
+    @IBOutlet weak var percpStepper: UIStepper!
+    @IBOutlet weak var luckStepper: UIStepper!
+    
+    @IBOutlet weak var strengthLabel: UILabel!
+    @IBOutlet weak var intelLabel: UILabel!
+    @IBOutlet weak var dexLabel: UILabel!
+    @IBOutlet weak var percpLabel: UILabel!
+    @IBOutlet weak var luckLabel: UILabel!
+    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var mpLabel: UILabel!
+    @IBOutlet weak var dodgeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBAction func updateStats(_ sender: UIStepper) {
+    }
+    func updateName(){
+        
+        nameLabel.text = nameField.text
 
+    }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            updateName()
+        }
+    
     override func viewDidLoad() {
+        nameField.delegate=self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
