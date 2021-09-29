e56409cc55b66874f43d5e0faf5fd0a1f488e408
trje9656 mac <trje9656@colorado.edu>
2021-09-29 15:51:29 -0600

    Initial Commit

---
+++ Lab4_RPGCharacterMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4_RPGCharacterMaker
+//
+//  Created by Trevor Jedziniak on 9/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
