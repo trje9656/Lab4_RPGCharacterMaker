3791db691aa4ce372e207b6947cb24861eb51591
trje9656 mac <trje9656@colorado.edu>
2021-09-29 17:01:04 -0600

    added keyboard tap out logic

--- Lab4_RPGCharacterMaker/ViewController.swift
+++ Lab4_RPGCharacterMaker/ViewController.swift
@@ -48,9 +48,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         nameField.delegate=self
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+         NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(self.dismissKeyboard))
+         view.addGestureRecognizer(tap)
+        
         // Do any additional setup after loading the view.
     }
-
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    @objc func keyboardWillShow(notification: NSNotification) {
+     if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+     if self.view.frame.origin.y == 0 {
+     self.view.frame.origin.y -= keyboardSize.height
+     }
+     }
+     }
+     @objc func keyboardWillHide(notification: NSNotification) {
+     if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue)
+    != nil {
+     if self.view.frame.origin.y != 0 {
+     self.view.frame.origin.y = 0
+     }
+     }
+     }
 
 }
 
